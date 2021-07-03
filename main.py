249cf453eb7158526ef4bf20727a50a8c135cbc6
Vinh Ngo Quang <vinh.neu@gmail.com>
2021-07-03 22:49:06 +0700

    Add content

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import requests
 from bs4 import BeautifulSoup
 URL = 'https://trangvangvietnam.com/categories/99110/phan-bon-cac-dai-ly-phan-bon.html'
 page = requests.get(URL)
-print(page)
+print(page.content)
